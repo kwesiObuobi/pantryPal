8347e9aa77456f325ab2fc8b8071ad0350c758f8
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-09-13 09:49:25 +0000

    Add google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div>
-      <h2>welcome</h2>
+      <h2>welcome to movies</h2>
     </div>
   )
 }
 
-export default App
+export default App;
