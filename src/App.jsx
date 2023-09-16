6d2d2e410ff931dfeb406f349e5ceca25dda0b85
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-09-16 10:53:19 +0000

    Get movie poster_path base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Headline from './components/App/headline/Headline';
 
 function App() {
+  const [movies, setMovies] = useState({});
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTUyYmFjYzM2MDlkZGE1ZTBkMWZjN2I3MTAyMWIxYiIsInN1YiI6IjY1MDE3ZjI5MWJmMjY2MDBjNWQ2NWYyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gtjGoyNC3GdOiT5p733uXPgyb4ZB3eFOB7IjBGhsjrY'
+      }
+    };
+  
+    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => setMovies(response))
+    .catch(err => setError(err));
+   }, []);
+
+   if (error) {
+    return (<h1>{error}</h1>)
+   }
+
+   if (!movies || !movies.results || movies.results.length === 0) {
+    return (<h1>Loading...</h1>)
+   }
+
   return (
     <div className="App">
-      <Headline />
+      <Headline movie={movies.results[0]} />
     </div>
   )
 }
--- src/components/App/headline/Headline.jsx
+++ src/components/App/headline/Headline.jsx
@@ -2,23 +2,31 @@ import './headline.css';
 import Logo from '../../../assets/images/tv.png';
 import Menu from '../../../assets/images/Menu.png';
 
-const Headline = () => {
+// eslint-disable-next-line react/prop-types
+const Headline = ({ movie }) => {
+  const IMAGEURL = 'https://image.tmdb.org/t/p/w500';
   return (
     <div className="headline-div">
-      <div className="header">
-        <div className="brand">
-          <div className="brand-img">
-            <img src={Logo} alt="MovieBox Logo" />
+      {/* eslint-disable-next-line react/prop-types */}
+      <img src={`${IMAGEURL}/${movie.poster_path}`} alt="poster path" className="headline-background" />
+
+      <div className="headline-content">
+        <div className="header">
+          <div className="brand">
+            <div className="brand-img">
+              <img src={Logo} alt="MovieBox Logo" />
+            </div>
+            {/* eslint-disable-next-line react/prop-types */}
+            <p className="brand-title">{movie.title}</p>
           </div>
-          <p className="brand-title">MovieBox</p>
-        </div>
-        <div className="search-div">
-          <input type="search" className="search-input" placeholder="What do you want to watch?" />
-        </div>
-        <div className="auth-box">
-          <p className="signin">Sign in</p>
-          <div className="hamburger-div">
-            <img src={Menu} alt="hamburger icon" />
+          <div className="search-div">
+            <input type="search" className="search-input" placeholder="What do you want to watch?" />
+          </div>
+          <div className="auth-box">
+            <p className="signin">Sign in</p>
+            <div className="hamburger-div">
+              <img src={Menu} alt="hamburger icon" />
+            </div>
           </div>
         </div>
       </div>
