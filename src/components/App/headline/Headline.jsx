e31468f506472a0d332b06710f7a65cfb5a3767b
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-09-16 11:33:56 +0000

    Add movie headline movie title and overview

--- src/components/App/headline/Headline.jsx
+++ src/components/App/headline/Headline.jsx
@@ -2,22 +2,19 @@ import './headline.css';
 import Logo from '../../../assets/images/tv.png';
 import Menu from '../../../assets/images/Menu.png';
 
-// eslint-disable-next-line react/prop-types
 const Headline = ({ movie }) => {
   const IMAGEURL = 'https://image.tmdb.org/t/p/w500';
   return (
     <div className="headline-div">
-      {/* eslint-disable-next-line react/prop-types */}
       <img src={`${IMAGEURL}/${movie.backdrop_path}`} alt="poster path" className="headline-background" />
 
-      <div className="headline-content">
+      <div className="container headline-content">
         <div className="header">
           <div className="brand">
             <div className="brand-img">
               <img src={Logo} alt="MovieBox Logo" />
             </div>
-            {/* eslint-disable-next-line react/prop-types */}
-            <p className="brand-title">{movie.title}</p>
+            <p className="brand-title">MovieBox</p>
           </div>
           <div className="search-div">
             <input type="search" className="search-input" placeholder="What do you want to watch?" />
@@ -28,6 +25,11 @@ const Headline = ({ movie }) => {
               <img src={Menu} alt="hamburger icon" />
             </div>
           </div>
+        </div>
+
+        <div className="headline-movie">
+          <p className="title">{movie.title}</p>
+          <p>{movie.overview}</p>
         </div>
       </div>
     </div>
