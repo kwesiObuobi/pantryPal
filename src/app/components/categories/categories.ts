e89f452041fa80be4da84076639769bacc1dc1e4
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2025-10-25 10:24:35 +0000

    Remove placeholder 'categories' and add dynamic data

--- src/app/components/categories/categories.ts
+++ src/app/components/categories/categories.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CategoryItem } from "../category-item/category-item";
+import { TheMealDb } from '../../services/the-meal-db';
 
 @Component({
   selector: 'app-categories',
@@ -8,5 +9,11 @@ import { CategoryItem } from "../category-item/category-item";
   styleUrl: './categories.css'
 })
 export class Categories {
-
+  categories = signal<any[]>([]);
+  loading = signal<boolean>(true);
+  private mealDbService = inject(TheMealDb);
+  async ngOnInit() {
+    this.categories.set(await this.mealDbService.getAllCategories())
+    this.loading.set(false);
+  }
 }
--- src/app/services/the-meal-db.ts
+++ src/app/services/the-meal-db.ts
@@ -7,6 +7,6 @@ export class TheMealDb {
   async getAllCategories() {
     const res = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
     const data = await res.json();
-    return data;
+    return data.categories;
   }
 }
