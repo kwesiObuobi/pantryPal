d07f36d065a2f0c3ad00eade64695aecb11df24f
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2025-10-26 20:29:19 +0000

    Add random meal feature

--- src/app/components/hero-section/hero-section.ts
+++ src/app/components/hero-section/hero-section.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TheMealDb } from '../../services/the-meal-db';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './hero-section.css'
 })
 export class HeroSection {
+  private mealDbService = inject(TheMealDb);
+  private router = inject(Router)
+  itemId = signal('');
 
+  async handleButtonClick() {
+    const meal = await this.mealDbService.getRandomMeal();
+    this.itemId.set(meal['idMeal'])
+    this.router.navigate(['/meal', this.itemId()])
+  }
 }
--- src/app/services/the-meal-db.ts
+++ src/app/services/the-meal-db.ts
@@ -21,4 +21,10 @@ export class TheMealDb {
     const data = await res.json();
     return data.meals[0];
   }
+
+  async getRandomMeal() {
+    const res = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const data = await res.json();
+    return data.meals[0];
+  }
 }
